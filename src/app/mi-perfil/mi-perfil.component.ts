5d2ac46c63e45009f86491425f2fdba4c13d5131
Abel <abeltrece@gmail.com>
2022-08-20 17:15:31 -0500

    SE AAGREGGO LLA FFOFTO

--- src/app/mi-perfil/mi-perfil.component.ts
+++ src/app/mi-perfil/mi-perfil.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiPerfilComponent implements OnInit {
 
+  imgPerfil = './assets/perfil.jpg';
   constructor() { }
 
   ngOnInit(): void {
